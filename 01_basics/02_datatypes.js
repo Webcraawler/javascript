3bb47455e469fafdb68b99fd721f4088b21eacfc
Zeeshan <92221305+Webcraawler@users.noreply.github.com>
2024-06-03 18:56:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs not  browser
+
+
+//generally 'let' keyword is used 
+
+let name = "raju" //string 
+let age = 2 //int
+let isValid = true //bool
+
+//other datatypes 
+
+//number -> range 2 to power 53
+//bigInt
+//string -> ""
+//boolean
+//null ->standalone value
+//undefined
+//symbol ->used for uniqueness will be used in react
+
+console.log(typeof age)
+console.log(typeof null) //object type
+console.log(typeof undefined)
