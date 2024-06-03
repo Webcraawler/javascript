fee2447c611b1995a657499446c9ee558e5cef4b
Zeeshan <92221305+Webcraawler@users.noreply.github.com>
2024-06-03 18:35:23 +0000

    variable and constant

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accId = 1234
+let accEmail = "abc@gmail.com"
+var accPass = "1234"
+
+accCity = "delhi"
+
+// accId = 2
+
+accEmail = "xyz@gmail.com"
+accPass = "2134"
+accCity = "pune"
+let accState
+
+// console.log(accPass)
+// console.log(accCity)
+// console.log(accEmail);
+
+console.table([accId,accEmail,accPass,accCity,accState])
