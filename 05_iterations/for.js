641916dd75333d8e99b544d76f0381f4b64c6def
Zeeshan <92221305+Webcraawler@users.noreply.github.com>
2024-06-07 17:55:19 +0000

    for

---
+++ 05_iterations/for.js
@@ -0,0 +1,9 @@
+//same as cpp
+
+let myarray = [1,232,55,4,6]
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    console.log(element)
+}
+
+//break and continue logic
