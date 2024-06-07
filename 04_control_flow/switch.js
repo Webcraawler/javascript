be8088e6fcfc0bfcc9216d97a43580110c29c298
Zeeshan <92221305+Webcraawler@users.noreply.github.com>
2024-06-07 15:56:59 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,22 @@
+//switch statement - if we need multiple condition checking then rather than using multiple if statement we use switch statements
+
+const month = 5
+
+switch(month)
+{
+    case 1:
+        console.log("january")
+        break;
+    
+    case 2:
+        console.log("february")
+        break;
+    
+    case 3:
+        console.log("march")
+        break;
+
+    default:
+        console.log("wrong input")
+        break;
+}
